4b6e32e5daae528d66c429dfe0407ced9ce43f9c
p49dev <formulaesp523@gmail.com>
2026-06-20 09:39:21 +0700

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@
 
 // ───── CONFIG ─────
 // Замени на реальный адрес твоего Railway-сервиса
-const BACKEND = 'libertyformula-production-4740.up.railway.app';
+const BACKEND = 'libertyformula-production.up.railway.app';
 const OPENF1  = 'https://api.openf1.org/v1';
 
 // ───── ROLE SWITCHING ─────
